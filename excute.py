ffef7259f38f17d30270f29bb1b4856936f625de
congminh869 <69363249+congminh869@users.noreply.github.com>
2021-01-05 19:17:18 +0700

    Update excute.py

--- excute.py
+++ excute.py
@@ -1,1 +1,4 @@
-dddd
+from pathlib import Path
+# code_path=Path('E:\Google Driver')
+parameter_file=open("/home/pi/audio/testAI/parameter.txt",'r')
+code_path=parameter_file.readline().splitlines()[0]
